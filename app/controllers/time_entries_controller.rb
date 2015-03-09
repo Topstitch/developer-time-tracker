1d56c2391625175369536ea432067d2a1ec1fe92
Topstitch <kallen@spoonflower.com>
2015-03-09 19:46:14 -0400

    TimeEntries controllers tests pass

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -1,11 +1,20 @@
 class TimeEntriesController < ApplicationController
+  before_action :set_time_entry, only: [:show, :edit, :update, :destroy]
+
   def index
+    @time_entries = TimeEntry.all
   end
 
   def new
   end
 
   def create
+    @time_entry = TimeEntry.new(time_entry_params)
+    if @time_entry.save
+      redirect_to time_entries_path, notice: 'Time Entry was successfully created.'
+    else
+      render :new
+    end
   end
 
   def show
@@ -15,8 +24,26 @@ class TimeEntriesController < ApplicationController
   end
 
   def update
+    if @time_entry.update(time_entry_params)
+      redirect_to time_entries_path, notice: 'Time Entry was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @time_entry.destroy
+    redirect_to time_entries_path, notice: 'Time Entry was successfully deleted.'
   end
+
+  private
+
+  def set_time_entry
+    @time_entry = TimeEntry.find(params[:id])
+  end
+
+  def time_entry_params
+    params.require(:time_entry).permit(:name, :email, :password)
+  end
+
 end
