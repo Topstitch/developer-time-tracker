7ca71e461ba7c6ca4641fbacc3b165d76ce34d6d
Topstitch <kallen@spoonflower.com>
2015-03-11 14:14:16 -0400

    Total hours works

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,12 @@ class Project < ActiveRecord::Base
   has_many :time_entries
   validates :max_hours, :name, presence: true
   validates :name, uniqueness: true
+
+  def total_hours
+    all_hours = []
+    time_entries.all.each do |t|
+      all_hours << t.duration
+    end
+    all_hours.reduce(:+)
+  end
 end
